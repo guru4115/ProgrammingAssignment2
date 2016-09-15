27d51df43364daf1a0d08e4b3120ceeb54b26442
guru4115 <n404115@gmail.com>
2016-09-15 09:01:36 +0800

    completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## The purpose of these functions are to creates a matrix 'x'
+## and calculate the inverse of the matrix. The computed value(matrix inverse) 
+## is placed in cache. when the function is ran again cache is checked.
+## data from cache 'm'is return if available
+#.................................................................................#
+# The Function makeCacheMatrix(x) creates a matrix 'x' and a cache 'm'
+# m is initialize with NA and reseted NA if a new x is inserted
+# and the cache NA when a new x is inserted
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- matrix() # x is initialize with NA 
+  set <- function(y) {
+    x <<- y
+    m <<- matrix() # reset cache with NA   
+  }
+  get <- function() x # return  x
+  setmatrix <- function(matrix) m <<- matrix #set the cache
+  getmatrix <- function() m # return cached data
+  list(set = set, get = get, # enable the use of $ operator
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# This function calculate the inverse of the matrix x 
+# returned by makeCacheMatrix above.  
+# If the inverse is available in cache the data is return from cache
+
+cacheSolve <- function(x, ...) 
+{
+  m <- x$getmatrix()
+  if(all(!is.na (m)))# check if data is in cache
+  {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...) # inverse x
+  x$setmatrix(m) # set cache
+  m      
 }
